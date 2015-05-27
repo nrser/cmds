4a395466417674a483dbfdc8e6d5dcbd9f69286c
nrser <neil@ztkae.com>
2015-05-28 07:21:53 +0800

    satisfied Cmds.expand_option_hash works to this point

--- spec/cmds/expand_option_hash_spec.rb
+++ spec/cmds/expand_option_hash_spec.rb
@@ -23,9 +23,39 @@ describe "Cmds.expand_option_hash_spec" do
   end # multiple single char keys
 
   context "one longer key" do
-    it "handled nil value" do
+    it "handles nil value" do
       expect(Cmds.expand_option_hash blah: nil).to eq "--blah"
     end
+
+    it "handles a simple value" do
+      expect(Cmds.expand_option_hash blah: 1).to eq "--blah=1"
+    end
+
+    it "handles an array value" do
+      expect(Cmds.expand_option_hash blah: [1, 2, 3]).to eq "--blah=1 --blah=2 --blah=3"
+    end
   end # one longer key
+
+  context "multiple longer keys" do
+    it "order expansion by key" do
+      expect(Cmds.expand_option_hash bob: 2, al: 1, cat: 3).to eq "--al=1 --bob=2 --cat=3"
+    end
+  end # multiple longer keys
+
+  it "handles a mess of stuff" do
+    expect(
+      Cmds.expand_option_hash d:    1,
+                              blah: "blow", 
+                              cat:  nil, 
+                              x:    ['m', 'e',]
+    ).to eq "--blah=blow --cat -d 1 -x m -x e"
+  end
+
+  it "escapes paths" do
+    expect(
+      Cmds.expand_option_hash path: "/some folder/some where",
+                              p:    "maybe ov/er here..."
+    ).to eq '-p maybe\ ov/er\ here... --path=/some\ folder/some\ where'
+  end
 
 end # ::expand_option_hash_spec
