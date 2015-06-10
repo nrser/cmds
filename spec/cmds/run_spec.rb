1b7091c76a4028b6391d28ed5a9e407467db4c6a
nrser <neil@ztkae.com>
2015-06-10 18:24:11 +0800

    fiddling around with Cmds::run testing

--- spec/cmds/run_spec.rb
+++ spec/cmds/run_spec.rb
@@ -3,12 +3,7 @@ require 'json'
 require 'spec_helper'
 
 describe "Cmds::run" do
-  context "when echo_cmd.rb is called with 'hello world!'" do
-
-    let(:result) {
-      Cmds "./test/echo_cmd.rb %s", ["hello world!"]
-    }
-
+  shared_examples "ok" do
     it "should be ok" do
       expect( result.ok? ).to be true
     end
@@ -16,9 +11,39 @@ describe "Cmds::run" do
     it "should have empty err" do
       expect( result.err ).to eq ""
     end
+  end # ok
 
-    it "should have 'hello world!' as ARGV[0]" do
-      expect( JSON.load(result.out)['ARGV'][0] ).to eq "hello world!"
+  context "echo_cmd.rb 'hello world!'" do
+
+    shared_examples "executes correctly" do
+      it_behaves_like "ok"
+
+      it "should have 'hello world!' as ARGV[0]" do
+        expect( JSON.load(result.out)['ARGV'][0] ).to eq "hello world!"
+      end
+    end # executes correctly
+
+    context "positional args" do
+      let(:result) {
+        Cmds "./test/echo_cmd.rb %s", ["hello world!"]
+      }
+
+      it_behaves_like "executes correctly"
     end
+
+    context "keyword args" do
+      let(:result) {
+        Cmds "./test/echo_cmd.rb %{s}", s: "hello world!"
+      }
+
+      it_behaves_like "executes correctly"
+    end
+
+  end # context echo_cmd.rb 'hello world!'
+
+  it "should error when second (subs) arg is not a hash or array" do
+    expect {
+      Cmds "./test/echo_cmd.rb %s", "hello world!"
+    }.to raise_error TypeError
   end
-end
+end # Cmds::run
