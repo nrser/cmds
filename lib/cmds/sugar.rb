1e9122c7f28946174c2d1bdcfe5543cb2f598c7d
nrser <neil@ztkae.com>
2015-10-14 16:29:07 +0800

    bump to 0.0.8, fix #3
    - added `.out`, `.out!`, `#out` and `#out!` to get stdout
    - added `.err` and `#err` to get stderr

--- lib/cmds/sugar.rb
+++ lib/cmds/sugar.rb
@@ -51,6 +51,57 @@ class Cmds
     Cmds.new(template, assert: true).stream *subs, &input_block
   end # ::stream!
 
+
+  # @api sugar
+  #
+  # captures and returns stdout (sugar for `Cmds.capture(*args).out`).
+  #
+  # @param template [String] see {.capture}.
+  # @param subs [Array] see {.capture}.
+  # @param input_block [Proc] see {.capture}.
+  #
+  # @see .capture
+  # @see Result#out
+  #
+  def self.out template, *subs, &input_block
+    capture(template, *subs, &input_block).out
+  end
+
+
+  # @api sugar
+  #
+  # captures and returns stdout, raising an error if the command fails.
+  #
+  # @param template [String] see {.capture}.
+  # @param subs [Array] see {.capture}.
+  # @param input_block [Proc] see {.capture}.
+  #
+  # @see .capture
+  # @see Result#out
+  #
+  def self.out! template, *subs, &input_block
+    Cmds.new(
+      template,
+      options(subs, input_block).merge!(assert: true),
+    ).capture.out
+  end
+
+
+  # @api sugar
+  #
+  # captures and returns stderr (sugar for `Cmds.capture(*args).err`).
+  #
+  # @param template [String] see {.capture}.
+  # @param subs [Array] see {.capture}.
+  # @param input_block [Proc] see {.capture}.
+  #
+  # @see .capture
+  # @see Result#err
+  #
+  def self.err template, *subs, &input_block
+    capture(template, *subs, &input_block).err
+  end
+
   # instance methods
   # ================
 
@@ -72,6 +123,63 @@ class Cmds
     stream do |io|
       io.in = $stdin
     end
+  end
+
+
+  # @api sugar
+  #
+  # captures and returns stdout
+  # (sugar for `#capture(*subs, &input_block).out`).
+  #
+  # @param subs [Array] see {.capture}.
+  # @param input_block [Proc] see {.capture}.
+  #
+  # @return [String] the command's stdout.
+  #
+  # @see #capture
+  # @see Result#out
+  #
+  def out *subs, &input_block
+    capture(*subs, &input_block).out
+  end
+
+
+  # @api sugar
+  #
+  # captures and returns stdout
+  # (sugar for `#capture(*subs, &input_block).out`).
+  #
+  # @param subs [Array] see {.capture}.
+  # @param input_block [Proc] see {.capture}.
+  #
+  # @return [String] the command's stdout.
+  #
+  # @see #capture
+  # @see Result#out
+  #
+  def out! *subs, &input_block
+    self.class.new(
+      @template,
+      merge_options(subs, input_block).merge!(assert: true),
+    ).capture.out
+  end
+
+
+  # @api sugar
+  #
+  # captures and returns stdout
+  # (sugar for `#capture(*subs, &input_block).err`).
+  #
+  # @param subs [Array] see {.capture}.
+  # @param input_block [Proc] see {.capture}.
+  #
+  # @return [String] the command's stdout.
+  #
+  # @see #capture
+  # @see Result#err
+  #
+  def err *subs, &input_block
+    capture(*subs, &input_block).err
   end
 
 end # class Cmds
